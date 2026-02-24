89213dbf27d87bd740b02ded39109b8fa26deb65
panks <p.panconi@gmail.com>
2026-02-25 00:17:48 +0100

    added flakes configuration

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+    };
+}
