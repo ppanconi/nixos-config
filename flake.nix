93111b0222c8052755d04beb664a18f630e78334
panks <p.panconi@gmail.com>
2026-05-13 20:04:56 +0200

    added my ocgo program

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hyprland.url = "github:hyprwm/Hyprland/v0.54.2";
+    ocgo.url = "github:ppanconi/ocgo/v1.0.1";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ocgo, ... }:
     let
       system = "x86_64-linux";
     in
@@ -27,6 +28,11 @@
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "hm-backup";
             home-manager.users.panks = import ./home.nix;
+          }
+          {
+            environment.systemPackages = [
+              ocgo.packages.x86_64-linux.default
+            ];
           }
         ];
       };
